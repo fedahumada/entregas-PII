a7c01d237a302c94dc8e0ab1ad0e81bc111317b0
Federico Ahumada <ahufede@gmail.com>
2022-10-15 12:15:27 -0300

    apis

---
+++ Apis/WebAPITest/Controllers/FechaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPITest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FechaController : ControllerBase
+    {
+
+
+
+    }
+}
---
+++ Apis/WebAPITest/Models/Fecha.cs
@@ -0,0 +1,25 @@
+﻿namespace WebAPITest.Models
+{
+    public class Fecha
+    {
+        public int Dia { get; set; }    
+
+        public string DiaSemana { get; set; }
+
+        public int Mes { get; set; }
+
+        public int Anio { get; set; }
+
+        public Fecha()
+        {
+            Dia = DateTime.Now.Day;
+            DiaSemana = DateTime.Now.DayOfWeek.ToString();
+            Mes = DateTime.Now.Month;
+            Anio = DateTime.Now.Year;
+
+        }
+
+
+
+    }
+}
